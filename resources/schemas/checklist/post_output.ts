2b7c3a4167b6069bb352594f5ba811fd98da3fc7
neidajessicatrujillodelgadillo1 <trujillodelgadilloneidajessica@gmail.com>
2025-10-05 13:46:15 -0400

    use schemas

--- resources/schemas/checklist/post_output.ts
+++ resources/schemas/checklist/post_output.ts
@@ -8,18 +8,7 @@ export const output_create_checklist = {
     pos: { type: "number" },
     checkItems: {
       type: "array",
-      items: {
-        type: "object",
-        properties: {
-          id: { type: "string" },
-          name: { type: "string" },
-          state: { type: "string" }
-        },
-        required: ["id", "name", "state"],
-        additionalProperties: false
-      }
-    }
+    },
   },
-  required: ["id", "name", "idBoard", "idCard", "pos", "checkItems"],
-  additionalProperties: false
+  required: ["id", "name", "idBoard", "idCard"],
 } as const;
